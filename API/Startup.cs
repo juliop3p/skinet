81ba2678de473288b32d6b03ca12e09c7f153a7b
Julio Cesar <julio15.zn@gmail.com>
2021-05-13 21:20:41 -0300

    End of section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,17 +1,12 @@
-using System.Linq;
-using API.Errors;
+using API.Extensions;
 using API.Helpers;
 using API.Middleware;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -30,38 +25,14 @@ namespace API
       services.AddDbContext<StoreContext>(x =>
         x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
-      // Injecting the repository
-      services.AddScoped<IProductRepository, ProductRepository>();
-
-      // Injecting the Generic Repository
-      services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-
       // Injecting AutoMapper
       services.AddAutoMapper(typeof(MappingProfiles));
 
       services.AddControllers();
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-      });
 
-      services.Configure<ApiBehaviorOptions>(options =>
-      {
-        options.InvalidModelStateResponseFactory = ActionContext =>
-        {
-          var errors = ActionContext.ModelState
-            .Where(e => e.Value.Errors.Count > 0)
-            .SelectMany(x => x.Value.Errors)
-            .Select(x => x.ErrorMessage).ToArray();
-
-          var errorResponse = new ApiValidationErrorResponse
-          {
-            Errors = errors
-          };
-
-          return new BadRequestObjectResult(errorResponse);
-        };
-      });
+      // Extensions Methods
+      services.AddApplicationServices();
+      services.AddSwaggerDocumentation();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -69,13 +40,6 @@ namespace API
     {
       // Error Handling
       app.UseMiddleware<ExceptionMiddleware>();
-
-      if (env.IsDevelopment())
-      {
-        // app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-      }
 
       app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
@@ -87,6 +51,8 @@ namespace API
       app.UseStaticFiles();
 
       app.UseAuthorization();
+
+      app.UseSwaggerDocumentation();
 
       app.UseEndpoints(endpoints =>
       {
